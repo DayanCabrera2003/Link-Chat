92a9975f0d61eda02acc0040d52a228b406f0743
DayanCabrera2003 <dayancorvo@gmail.com>
2025-10-06 17:55:48 -0400

    Empezando de cero

--- mensajeria-capa-enlace/src/checksum.py
+++
@@ -1,8 +0,0 @@
-
-def calcular_checksum(data):
-    """Calcula un checksum simple sumando los valores ASCII de los bytes."""
-    return sum(data) % 256
-
-def verificar_checksum(data, checksum):
-    """Verifica si el checksum calculado coincide con el proporcionado."""
-    return calcular_checksum(data) == checksum
